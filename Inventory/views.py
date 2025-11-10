7557b80a2c86a8f1bd1ad536f141d673d579dd83
Balamurugan <balamuruganelangovan.official@gmail.com>
2025-11-10 23:02:40 +0530

    if elif added

--- Inventory/views.py
+++ Inventory/views.py
@@ -3,5 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 
 def HomePage(request):
-    return render(request,'index.html')
+    context={
+        "name":"Balamurugan",
+        "role":"Manager",
+        "numbers":[1,2,3,4,5], 
+        "marks":{
+            "Tamil":100,
+            "English":100,
+        }
+    }
+    return render(request,'index.html',context)
  
