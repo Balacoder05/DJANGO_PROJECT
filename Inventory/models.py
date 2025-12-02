c370864ea10c3d77721b6151bf818b3873978c0c
Balamurugan <balamuruganelangovan.official@gmail.com>
2025-12-02 17:45:08 +0530

    creating database

--- Inventory/models.py
+++ Inventory/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+
+    Product_name=models.CharField(max_length=200,null=True)
+    Product_code=models.CharField(max_length=200,null=True)
+    Price=models.FloatField(default=0)
+    Gst=models.IntegerField(default=0) 
