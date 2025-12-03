094d5f3312b10d64337f1fbca7f72e7c805fd900
Balamurugan <balamuruganelangovan.official@gmail.com>
2025-12-03 22:08:02 +0530

    Admin panel access

--- Inventory/models.py
+++ Inventory/models.py
@@ -8,3 +8,7 @@ class Product(models.Model):
     Product_code=models.CharField(max_length=200,null=True)
     Price=models.FloatField(default=0)
     Gst=models.IntegerField(default=0) 
+    Food_Product=models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.Product_name
